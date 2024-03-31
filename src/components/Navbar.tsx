dcc46d2d55bdc6b2c60ea3da8f7fb2f1bc2433af
akotonystark <alarbiampofo@gmail.com>
2024-03-31 01:16:11 -0400

    Flex Layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Box, Text, Button, Flex, Heading, Spacer, HStack } from "@chakra-ui/react"
+
+const Navbar = () => {
+    return (
+        <Flex as={"nav"} p={10} alignItems={'center'} gap={5}>
+            <Heading>Task Manager</Heading>
+            <Spacer />
+
+            <HStack spacing={'20px'}>
+                <Box bg={'gray.200'} p={10}>A</Box>
+                <Text>user@gmail.com</Text>
+                <Button colorScheme='teal'>Logout</Button>
+            </HStack>
+
+        </Flex>
+        // <Flex bg="gray.200" justify={'space-between'} wrap={'wrap'} gap={2}>
+        //     <Box w={150} h={100} bg={'red'}></Box>
+        //     <Box w={150} h={100} bg={'blue'}></Box>
+        //     <Box w={150} h={100} bg={'green'}></Box>
+        //     <Box w={150} h={100} bg={'yellow'}></Box>
+        // </Flex>
+    )
+}
+
+export default Navbar
